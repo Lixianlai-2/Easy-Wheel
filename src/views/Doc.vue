e9682e8a2bfcc5dcac184057762e1496773d6373
Li <1643565650@qq.com>
2022-08-14 22:21:35 +0800

    增加Switch组件内容，设置边栏和主要内容区域样式

--- src/views/Doc.vue
+++ src/views/Doc.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <Topnav />
+  <div class="layout">
+    <Topnav class="nav" />
     <div class="content">
       <aside v-show="asideVisible">
         <h2>组件列表</h2>
@@ -40,6 +40,7 @@ aside {
   top: 0;
   left: 0;
   padding-top: 70px;
+  height: 100%;
   > h2 {
     margin-bottom: 4px;
   }
@@ -54,5 +55,35 @@ aside {
     left: 0;
     padding-top: 70px;
   }
+}
+.layout {
+  display: flex;
+  flex-direction: column;
+  height: 100vh;
+  > .nav {
+    flex-shrink: 0;
+  }
+  > .content {
+    flex-grow: 1;
+    padding-top: 60px;
+    padding-left: 156px;
+    @media (max-width: 500px) {
+      padding-left: 0;
+    }
+  }
+}
+.content {
+  display: flex;
+  > aside {
+    flex-shrink: 0;
+  }
+  > main {
+    flex-grow: 1;
+    padding: 16px;
+    background: lightgreen;
+  }
+}
+main {
+  overflow: auto;
 }
 </style>
