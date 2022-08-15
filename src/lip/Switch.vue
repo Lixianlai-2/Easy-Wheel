ab52adf146c077f4e122def4a4e00113a17e4efa
Li <1643565650@qq.com>
2022-08-15 15:55:54 +0800

    实现switch按钮的切换效果

--- src/lip/Switch.vue
+++ src/lip/Switch.vue
@@ -1,11 +1,51 @@
 <template>
   <div>
-    <h1>这是switch组件</h1>
+    <button @click="toggle" :class="{ checked }"><span></span></button>
   </div>
 </template>
 
 <script>
-export default {};
+import { ref } from "@vue/reactivity";
+export default {
+  setup() {
+    let checked = ref(false);
+    let toggle = () => {
+      checked.value = !checked.value;
+    };
+    return {
+      checked,
+      toggle,
+    };
+  },
+};
 </script>
 
-<style scoped></style>
+<style lang="scss" scoped>
+$h: 22px;
+$h2: $h - 4px;
+button {
+  height: $h;
+  width: $h * 2;
+  border-radius: $h / 2;
+  position: relative;
+  border: none;
+  background-color: grey;
+  // background-color: #0d8cff;
+}
+span {
+  height: $h2;
+  width: $h2;
+  border-radius: $h2 / 2;
+  background-color: #fefefe;
+  position: absolute;
+  top: 2px;
+  left: 2px;
+}
+button.checked {
+  background-color: #0d8cff;
+}
+
+button.checked > span {
+  left: calc(100% - #{$h2} - 2px);
+}
+</style>
