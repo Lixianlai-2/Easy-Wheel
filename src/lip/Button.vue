f946cdf0623fe2948673ef45cf8e85299616d196
Li <1643565650@qq.com>
2022-08-16 23:17:00 +0800

    设置链接按钮和文本按钮的样式

--- src/lip/Button.vue
+++ src/lip/Button.vue
@@ -62,44 +62,25 @@ $radius: 4px;
   &::-moz-focus-inner {
     border: 0;
   }
+  // 设置链接按钮样式
+  &.gulu-theme-link {
+    border-color: transparent;
+    box-shadow: none;
+    color: $blue;
+    &:hover,
+    &focus {
+      color: lighten($blue, 10%);
+    }
+  }
+
+  &.gulu-theme-text {
+    border-color: transparent;
+    box-shadow: none;
+    color: inherit;
+    &:hover,
+    &:focus {
+      background: darken(white, 5%);
+    }
+  }
 }
-// .gulu-button {
-//   box-sizing: border-box;
-//   height: $h;
-//   padding: 0 12px;
-//   cursor: pointer;
-//   display: inline-flex;
-//   justify-content: center;
-//   align-items: center;
-//   white-space: nowrap;
-//   background: white;
-//   color: $color;
-//   border: 1px solid $border-color;
-//   border-radius: $radius;
-//   box-shadow: 0 1px 0 fade-out(black, 0.95);
-//   & + & {
-//     margin-left: 8px;
-//   }
-//   &:hover,
-//   &:focus {
-//     color: $blue;
-//     border-color: $blue;
-//   }
-//   &:focus {
-//     outline: none;
-//   }
-//   &::-moz-focus-inner {
-//     border: 0;
-//   }
-
-//   div {
-//     border: 4px solid blue;
-//     background-color: skyblue;
-//   }
-
-//   button {
-//     padding: 10px;
-//     border: 2px solid lightgreen;
-//   }
-// }
 </style>
