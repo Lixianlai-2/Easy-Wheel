b7ed9e84eeb9580756316f0a91d0b016912e1fbe
Li <1643565650@qq.com>
2022-08-16 11:23:02 +0800

    父组件传递属性和事件，取消默认绑定等

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -2,7 +2,10 @@
   <div>Button 示例</div>
   <h1>示例1</h1>
   <div>
-    <Button>你好</Button>
+    <!-- 绑定了三个事件，调用onClick函数 -->
+    <Button @click="onClick" @focus="onClick" @mouseover="onClick" :size="15"
+      >这是button按钮</Button
+    >
   </div>
 </template>
 
@@ -11,5 +14,16 @@ import Button from "../lip/Button.vue";
 
 export default {
   components: { Button },
+  setup() {
+    let onClick = function () {
+      console.log("works");
+    };
+    return {
+      onClick,
+    };
+  },
 };
 </script>
+
+<!-- 为什么函数不需要用ref或者reactive，因为我们要弄得是响应式数据啊 -->
+<!-- 怎么在vscode中设置快捷键，打印出console.log -->
--- src/lip/Button.vue
+++ src/lip/Button.vue
@@ -1,11 +1,49 @@
 <template>
-  <button>
-    <slot />
-  </button>
+  <!-- 绑定动态属性size -->
+  <div :size="size">
+    <!-- 直接绑定从父组件传来的属性和事件 -->
+    <button v-bind="$attrs">
+      <!-- <button v-bind="rest"> -->
+      <slot />
+    </button>
+  </div>
+
+  <!-- <div class="这里是根元素">
+    <button>
+      <slot />
+    </button>
+  </div> -->
 </template>
 
 <script lang="ts">
-export default {};
+export default {
+  // 用于判断根元素继承属性,设置为否，继承的是从父组件传入的
+  inheritAttrs: false,
+  // props: ["onMouseover"],
+  setup(props, context) {
+    // 这里用到了展开运算符和剩余运算符,得到从父组件传递来的属性
+    const { size, ...rest } = context.attrs;
+    console.log(`rest`, rest);
+    //context.attrs得到的是写什么数据？得到的是，未经过Props接收的，从父组件传递过来的属性
+    console.log(`context.attrs@@`, context.attrs);
+    return {
+      size,
+      rest,
+    };
+  },
+};
 </script>
 
-<style scoped></style>
+<style scoped>
+div {
+  border: 4px solid blue;
+  background-color: skyblue;
+}
+
+button {
+  padding: 10px;
+  border: 2px solid lightgreen;
+}
+</style>
+
+<!-- v-bind的缩写是: 那么v-bind='rest' 代表着什么？ :rest ? -->
