cecf29869cd9f61d27bf93ea4d6ed1cf88596669
Li <1643565650@qq.com>
2022-08-17 11:33:46 +0800

    添加大中小按钮及其样式

--- src/components/ButtonDemo.vue
+++ src/components/ButtonDemo.vue
@@ -9,10 +9,22 @@
   </div>
   <h1>示例2</h1>
   <div>
-    <!-- 添加大中小按钮 -->
-    <Button size="big">大</Button>
-    <Button>普通</Button>
-    <Button size="small">小</Button>
+    <!-- 添加大中小 普通按钮 -->
+    <Button size="big">大大大</Button>
+    <Button>普普普</Button>
+    <Button size="small">小小小</Button>
+  </div>
+  <div>
+    <!-- 添加大中小 文本按钮 -->
+    <Button theme="text" size="big">大大大</Button>
+    <Button theme="text">普普普</Button>
+    <Button theme="text" size="small">小小小</Button>
+  </div>
+  <div>
+    <!-- 添加大中小 链接按钮 -->
+    <Button theme="link" size="big">大大大</Button>
+    <Button theme="link">普普普</Button>
+    <Button theme="link" size="small">小小小</Button>
   </div>
 </template>
 
