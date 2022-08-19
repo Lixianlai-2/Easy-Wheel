4cb3dafacc639b7764ab32e3e7725a74451da55a
Li <1643565650@qq.com>
2022-08-19 12:11:48 +0800

    添加独特的class，如gulu-dialog ，添加css样式

--- src/lip/Dialog.vue
+++ src/lip/Dialog.vue
@@ -1,16 +1,18 @@
 <template>
   <!-- 这部分内容是做mask-->
-  <div class="overlay"></div>
-  <div class="wrapper">
-    <header>标题</header>
-    <main>
-      <p>第一行内容</p>
-      <p>第二行内容</p>
-    </main>
-    <footer>
-      <Button>确认</Button>
-      <Button>取消</Button>
-    </footer>
+  <div class="gulu-dialog-overlay"></div>
+  <div class="gulu-dialog-wrapper">
+    <div class="gulu-dialog">
+      <header>标题 <span class="gulu-dialog-close"></span></header>
+      <main>
+        <p>第一行内容</p>
+        <p>第二行内容</p>
+      </main>
+      <footer>
+        <Button level="main">确认</Button>
+        <Button>取消</Button>
+      </footer>
+    </div>
   </div>
 </template>
 
@@ -19,4 +21,82 @@ import Button from "./Button.vue";
 export default { components: { Button } };
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+// 设置基本变量，方便复用
+$radius: 4px;
+$border-color: #d9d9d9;
+// ----------------------------------
+.gulu-dialog {
+  background: white;
+  border-radius: $radius;
+  box-shadow: 0 0 3px fade_out(black, 0.5);
+  min-width: 15em;
+  max-width: 90%;
+
+  // 设置遮罩层
+  &-overlay {
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    background: fade_out(black, 0.5);
+    z-index: 10;
+  }
+  // 将弹窗相对viewport居中
+  &-wrapper {
+    position: fixed;
+    left: 50%;
+    top: 50%;
+    transform: translate(-50%, -50%);
+    z-index: 11;
+  }
+
+  > header {
+    padding: 12px 16px;
+    border-bottom: 1px solid $border-color;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    font-size: 20px;
+  }
+
+  > main {
+    padding: 12px 16px;
+  }
+
+  > footer {
+    border-top: 1px solid $border-color;
+    padding: 12px 16px;
+    text-align: right;
+  }
+
+  &-close {
+    position: relative;
+    display: inline-block;
+    width: 16px;
+    height: 16px;
+    cursor: pointer;
+
+    // :before 选择器向选定的元素前插入内容。
+    &::before,
+    &::after {
+      content: "";
+      position: absolute;
+      height: 1px;
+      background: black;
+      width: 100%;
+      top: 50%;
+      left: 50%;
+    }
+
+    &::before {
+      transform: translate(-50%, -50%) rotate(-45deg);
+    }
+
+    &::after {
+      transform: translate(-50%, -50%) rotate(45deg);
+    }
+  }
+}
+</style>
