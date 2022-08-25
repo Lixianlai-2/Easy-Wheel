168ebc104868195dbe5da8f9c180fef0f52233f0
Li <1643565650@qq.com>
2022-08-25 16:28:29 +0800

    引入并使用github-markdown-css，以实现markdown样式

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,7 @@
 // 注意要先引入css，再使用组件，不然覆盖顺序就错误了
 import "./index.scss";
 import "./lip/gulu.scss";
+import "../node_modules/github-markdown-css";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router";
